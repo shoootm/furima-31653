5756cd241c3ae061738e8291fddf8d8041fab409
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-14 17:50:30 +0900

    destroyアクション削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to:'items#index'
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show, :destroy]
+  resources :items, only: [:index, :new, :create, :show,]
 end
