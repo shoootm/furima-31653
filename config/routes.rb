1df7bb609ec17f5848ded18b7054ab3ee549e40d
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-18 12:12:25 +0900

    商品購入機能の実装"指摘された問題点修正"（２）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to:'items#index'
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :items do
     resources :purchases, only: [:index, :create]
   end
 end
