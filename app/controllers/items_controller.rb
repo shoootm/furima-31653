8a4ebc139035d17510f81b5b56c50959ae4523c1
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-20 17:08:21 +0900

    コメント機能修正（２）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @comments = Comment.all
+    @comments = @item.comments.includes(:user)
     @comment = Comment.new
   end
 
